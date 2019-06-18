74cd18f2d390e923c6dcbf1b80b71ba46fcb8c3d
Donovan Solms <donovan.solms@gmail.com>
2019-06-18 21:00:06 +0200

    Updated logrus package location

--- peer/peer.go
+++ peer/peer.go
@@ -14,8 +14,8 @@ import (
 	"sync"
 	"time"
 
-	_ "github.com/Sirupsen/logrus"
-	log "github.com/Sirupsen/logrus"
+	_ "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/donovansolms/ZeroGo/interfaces"
 	"github.com/donovansolms/ZeroGo/utils"
 	msgpack "gopkg.in/vmihailenco/msgpack.v2"
